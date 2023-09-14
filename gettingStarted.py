60f774a09b3c00274c8211b897a5da7d71a50db4
Shantnyu <144965598+Shantnyu@users.noreply.github.com>
2023-09-13 20:29:31 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -16,9 +16,8 @@ def welcome_assignment_answers(question):
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-    elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' " \
-                     "- Use SHA256 hash generator and use the answer in your code":
-        answer = "883c13da6a24949c9a23231b60119e2ace58459da4f8bbdd812cc37764548bdd"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "7939afe8e0e5542412d6bebe8e02f50496c412228b21c724ebfeacc750507cd9"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
@@ -52,8 +51,7 @@ if __name__ == "__main__":
     debug_question_5 = "Is a hashed message supposed to be un-hashed? - Yes/No"
     print(welcome_assignment_answers(debug_question_5))
 
-    debug_question_6 = "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' " \
-                     "- Use SHA256 hash generator and use the answer in your code"
+    debug_question_6 = "What is the SHA256 hashing value of your NYU email and use the answer in your code - "
     print(welcome_assignment_answers(debug_question_6))
 
     debug_question_7 = "Is MD5 a secured hashing algorithm? - Yes/No"
